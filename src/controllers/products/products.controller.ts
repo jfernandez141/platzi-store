def8e3b51e9851a5b6a8a20e24079de836a43839
Jhamil Fernandez <j-fernandez95@hotmail.com>
2024-02-04 16:44:35 -0500

    Added controllers

--- src/controllers/products/products.controller.ts
+++ src/controllers/products/products.controller.ts
@@ -1,13 +1,50 @@
-import { Controller, Get, Param, Query } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Param,
+  Post,
+  Query,
+  Body,
+  Put,
+  Delete,
+  HttpCode,
+  HttpStatus,
+} from '@nestjs/common';
 
 @Controller('products')
 export class ProductsController {
   @Get('/')
   getProducts(@Query('limit') limit = 100, @Query('offset') offset = 0) {
-    return `${limit}, ${offset}`;
+    return {
+      message: `${limit}, ${offset}`,
+    };
   }
+
   @Get('/:id')
+  @HttpCode(HttpStatus.ACCEPTED)
   getProduct(@Param('id') id: string) {
-    return `product ${id}`;
+    return { message: `product ${id}` };
+  }
+
+  @Post()
+  create(@Body() payload: any) {
+    return {
+      message: 'Accion de crear',
+      payload,
+    };
+  }
+
+  @Put(':id')
+  update(@Param('id') id: number, @Body() payload: any) {
+    return {
+      id,
+      payload,
+    };
+  }
+  @Delete(':id')
+  delete(@Param('id') id: number) {
+    return {
+      message: `Se elimino el producto con id ${id}`,
+    };
   }
 }
