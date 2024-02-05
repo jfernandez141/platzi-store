abb66037cc9639715f49ec90f882a1492ff5b48a
Jhamil Fernandez <j-fernandez95@hotmail.com>
2024-02-05 11:38:11 -0500

    added error handler

--- src/controllers/products/products.controller.ts
+++ src/controllers/products/products.controller.ts
@@ -10,36 +10,33 @@ import {
   HttpCode,
   HttpStatus,
 } from '@nestjs/common';
+import { ProductsService } from 'src/services/products/products.service';
 
 @Controller('products')
 export class ProductsController {
+  constructor(private productsService: ProductsService) {}
+
   @Get('/')
   getProducts(@Query('limit') limit = 100, @Query('offset') offset = 0) {
     return {
-      message: `${limit}, ${offset}`,
+      message: this.productsService.findAll(),
     };
   }
 
   @Get('/:id')
   @HttpCode(HttpStatus.ACCEPTED)
-  getProduct(@Param('id') id: string) {
-    return { message: `product ${id}` };
+  getProduct(@Param('id') id: number) {
+    return { message: this.productsService.findOne(id) };
   }
 
   @Post()
   create(@Body() payload: any) {
-    return {
-      message: 'Accion de crear',
-      payload,
-    };
+    return this.productsService.create(payload);
   }
 
   @Put(':id')
   update(@Param('id') id: number, @Body() payload: any) {
-    return {
-      id,
-      payload,
-    };
+    return this.productsService.update(id, payload);
   }
   @Delete(':id')
   delete(@Param('id') id: number) {
